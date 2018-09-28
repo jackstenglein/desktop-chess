b4106da2399f2d26e1b511fae25f99abf4b1fa42
Jack Stenglein <jackstenglein@utexas.edu>
2018-09-28 18:13:07 -0500

    Clean up Space.java

--- src/Space.java
+++ src/Space.java
@@ -2,120 +2,152 @@
 public class Space {
 	
 	//instance variables
-	private Piece piece_;
-	private int row_;
-	private int col_;
-	private boolean isSelected_;
-	private boolean isPossibleMove_;
-	private boolean isTakingMove_;
+	private Piece piece;
+	private int row;
+	private int col;
+	private boolean isSelected;
+	private boolean isPossibleMove;
+	private boolean isTakingMove;
 	
-	//initializes with row and col to specified values
-	//piece is set to null
-	//pre: Board.MIN_ROW <= row <= Board.MAX_ROW, Board.MIN_COL <= col <= Board.MAX_COL
-	public Space(int row, int col)
-	{
-		//check precondition
-		if(row < Board.MIN_ROW || row > Board.MAX_ROW || col < Board.MIN_COL || col > Board.MAX_COL)
+	/**
+	 * Creates a Space object with row and column set to the specified values 
+	 * and no piece contained within it.
+	 * 
+	 * @param row The row of the space.
+	 * 			  Board.MIN_ROW <= row <= Board.MAX_ROW
+	 * @param col The column of the space.
+	 * 			  Board.MIN_COL <= col <= Board.MAX_COL
+	 */
+	public Space(int row, int col) {
+		this(row, col, null);
+	}
+	
+	/**
+	 * Creates a Space object with row, column and piece set to the specified values.
+	 * 
+	 * @param row The row of the space.
+	 * 			  Board.MIN_ROW <= row <= Board.MAX_ROW
+	 * @param col The column of the space.
+	 * 			  Board.MIN_COL <= col <= Board.MAX_COL
+	 * @param piece The piece this space holds.
+	 */
+	public Space(int row, int col, Piece piece) {
+		if (row < Board.MIN_ROW || row > Board.MAX_ROW || col < Board.MIN_COL || col > Board.MAX_COL) {
 			throw new IllegalArgumentException("Row/col are out of bounds. Row: " + row + ", col: " + col);
-		
-		row_= row;
-		col_ = col;
-		piece_ = null;
+		}
+		this.row = row;
+		this.col = col;
+		this.piece = piece;
 	}
 	
-	//initializes with row, col and piece to specified values
-	//pre: Board.MIN_ROW <= row <= Board.MAX_ROW, Board.MIN_COL <= col <= Board.MAX_COL
-	public Space(int row, int col, Piece piece)
-	{
-		//check precondition
-		if(row < Board.MIN_ROW || row > Board.MAX_ROW || col < Board.MIN_COL || col > Board.MAX_COL)
-			throw new IllegalArgumentException("Row/col are out of bounds. Row: " + row + ", col: " + col);
-		
-		row_= row;
-		col_ = col;
-		piece_ = piece;
+	/**
+	 * Sets the space to selected or not selected.
+	 * 
+	 * @param selected A boolean indicating whether the space should
+	 *                 be rendered as selected.
+	 */
+	public void setSelected(boolean selected) {
+		isSelected = selected;
 	}
 	
-	//sets the space to selected or not selected
-	//pre: none
-	public void setSelected(boolean selected)
-	{
-		isSelected_ = selected;
+	/**
+	 * Returns whether this space should be rendered as
+	 * selected or not.
+	 * 
+	 * @return A boolean indicating whether the space is selected.
+	 */
+	public boolean isSelected() {
+		return isSelected;
 	}
 	
-	//returns the value of isSelected
-	//pre: none
-	public boolean isSelected()
-	{
-		return isSelected_;
+	/**
+	 * Sets the space to be displayed as a taking move or not.
+	 * 
+	 * @param isTakingMove A boolean indicating whether this space is a taking move.
+	 */
+	public void setTakingMove(boolean isTakingMove) {
+		this.isTakingMove = isTakingMove;
 	}
 	
-	//inform the space of whether or not it is a taking move
-	//pre: none
-	public void setTakingMove(boolean isTakingMove)
-	{
-		isTakingMove_ = isTakingMove;
+	/**
+	 * Returns whether this space should be rendered as a taking move
+	 * or not.
+	 * 
+	 * @return A boolean indicating whether the space is a taking move.
+	 */
+	public boolean isTakingMove() {
+		return isTakingMove;
 	}
 	
-	public boolean isTakingMove()
-	{
-		return isTakingMove_;
+	/**
+	 * Sets whether this space should be rendered as a possible move.
+	 * 
+	 * @param isPossibleMove A boolean indicating that this space is a possible move.
+	 */
+	public void setPossibleMove(boolean isPossibleMove) {
+		this.isPossibleMove = isPossibleMove;
 	}
 	
-	//inform the space of whether or not it is a possible move
-	//pre: none
-	public void setPossibleMove(boolean isPossibleMove)
-	{
-		isPossibleMove_ = isPossibleMove;
+	/**
+	 * Returns whether this space is a possible move.
+	 * 
+	 * @return A boolean indicating whether this space is a possible move.
+	 */
+	public boolean isPossibleMove() {
+		return isPossibleMove;
 	}
 	
-	//returns the value of isPossibleMove
-	//pre: none
-	public boolean isPossibleMove()
-	{
-		return isPossibleMove_;
+	/**
+	 * Sets the piece that this space is holding.
+	 * 
+	 * @param piece The piece to place in this space.
+	 */
+	public void setPiece(Piece piece) {
+		this.piece = piece;
 	}
 	
-	//sets the piece to the specified value, can be null
-	//pre: none
-	public void setPiece(Piece piece)
-	{
-		piece_ = piece;
+	/**
+	 * Returns the piece that this space is holding.
+	 * 
+	 * @return The piece contained in this space, or null
+	 *         if none exists.
+	 */
+	public Piece getPiece() {
+		return piece;
 	}
 	
-	//returns the piece
-	//pre: none
-	public Piece getPiece()
-	{
-		return piece_;
+	/**
+	 * Returns true if this space is empty and false otherwise.
+	 * 
+	 * @return True if this space is empty.
+	 */
+	public boolean isEmpty() {
+		return (piece == null);
 	}
 	
-	//returns whether the space is empty or not
-	//pre: none
-	public boolean isEmpty()
-	{
-		return (piece_ == null);
+	/**
+	 * Return the row of this space.
+	 * @return The row
+	 */
+	public int getRow() {
+		return row;
 	}
 	
-	//returns the row
-	//pre: none
-	public int getRow()
-	{
-		return row_;
+	/**
+	 * Return the column of this space.
+	 * @return The column
+	 */
+	public int getCol() {
+		return col;
 	}
 	
-	//returns the col
-	//pre: none
-	public int getCol()
-	{
-		return col_;
-	}
-	
-	//returns the row, col and piece concatenated in a string
-	//pre: none
-	public String toString()
-	{
-		String result = "Row: " + row_ + ", Col: " + col_ + "Piece " + piece_;
+	/**
+	 * Returns the row, column and piece concatenated in a string.
+	 * 
+	 * @return A string representation of this space.
+	 */
+	public String toString() {
+		String result = "Row: " + row + ", Col: " + col + "Piece " + piece;
 		return result;
 	}
 	
@@ -123,21 +155,24 @@ public class Space {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + col_;
-		result = prime * result + row_;
+		result = prime * result + col;
+		result = prime * result + row;
 		return result;
 	}
 
+	/**
+	 * Returns true if other is a Space object with the same row and
+	 * column as this object.
+	 * 
+	 * @param other The object to compare to this Space.
+	 * @return True if the objects are equal. False otherwise.
+	 */
 	public boolean equals(Object other) {
-		
 		if(other instanceof Space) {
-			//safe to cast
 			Space otherSpace = (Space)other;
-			
-			if(row_ == otherSpace.row_ && col_ == otherSpace.col_)
+			if(row == otherSpace.row && col == otherSpace.col)
 				return true;
 		}
-		
 		return false;
 	}
 }
